495a8b4a4b5eab7dedda80583504065f8564f104
Stefano Pavan <stefano.pavan.22@gmail.com>
2023-12-03 19:03:45 +0100

    Cmakelists done, package.xml done, exercise_4 calculate the positions of 3 people using only the callback function, exercise 4_1 instead stores the positions in an array and the performs the operations; same result.

--- src/exercise_4.cpp
+++ src/exercise_4.cpp
@@ -3,8 +3,14 @@
 #include "cmath"
 bool processed;
 ros::Subscriber sub;
-int leg_count;
-std::vector<std::pair<float, float>> person_positions;
+int leg_count=0;
+int person_count=0;
+int range_count=0;
+float mid_x[2];
+float mid_y[2];
+float person_x[3];
+float person_y[3];
+std::vector<std::pair<float, float>> leg_ranges[2];
 void positionCallback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
 {
     if (processed) 
@@ -15,47 +21,65 @@ void positionCallback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
     float angle_min = scan_msg->angle_min;
     float angle_increment = scan_msg->angle_increment;
 
-    for (size_t i = 0; i < ranges.size(); ++i) 
+    for (int i = 0; i < ranges.size(); i++) 
     {
         if (std::isinf(ranges[i])) 
         {
             continue;
         }
-        float angle = angle_min + (i * angle_increment);
-        float x = ranges[i] * std::cos(angle);
-        float y = ranges[i] * std::sin(angle);
+        else
+        {
+            float angle = angle_min + (i * angle_increment);
+            float x = ranges[i] * std::cos(angle);
+            float y = ranges[i] * std::sin(angle);
         
-        if (leg_count % 2 == 0) 
-        {
-            person_positions.push_back(std::make_pair(x, y));
-        } 
-        else 
-        {
-            float mid_x = (person_positions.back().first + x) / 2.0;
-            float mid_y = (person_positions.back().second + y) / 2.0;
+            leg_ranges[leg_count].push_back(std::make_pair(x, y));
+            range_count++;
 
-            ROS_INFO("Person %d Position: (x, y) = (%f, %f)", leg_count / 2 + 1, mid_x, mid_y);
-        }
+            if (std::isinf(ranges[i+1])) 
+            {
+                for(int j=0; j<= range_count;range_count++)
+                {
+                    mid_x[leg_count] += leg_ranges[leg_count][j].first;
+                    mid_y[leg_count] += leg_ranges[leg_count][j].second;
+                }
+                    mid_x[leg_count]= mid_x[leg_count]/range_count;
+                    mid_y[leg_count]= mid_y[leg_count]/range_count;
 
-        leg_count++;
+           // ROS_INFO("Person %u Position: (x, y) = (%f, %f)", leg_count + 1, mid_x, mid_y);
+                leg_ranges[leg_count].clear();
+                range_count = 0;
+                leg_count++;  
 
-        if (leg_count == 6) 
-        {
-            sub.shutdown();
-            processed = true;
-            break;
+                if (leg_count==2) 
+                {
+                    person_x[person_count]= (mid_x[leg_count-2]+ mid_x[leg_count-1])/2.0;
+                    person_y[person_count]= (mid_y[leg_count-2]+ mid_y[leg_count-1])/2.0;
+                    ROS_INFO("Person %u Position: (x, y) = (%f, %f)", person_count + 1, person_x[person_count], person_y[person_count]);
+                    leg_count=0;
+                    person_count++;
+                }
+                
+
+                if (person_count==3)
+                {
+                    sub.shutdown();
+                    processed = true;
+                    break;
+                } 
+            }
         }
     }
-    int num_ranges = scan_msg->ranges.size();
-    ROS_INFO("Range: %d.", num_ranges);
+    //int num_ranges = scan_msg->ranges.size();
+    //ROS_INFO("Range: %d.", num_ranges);
 
 }
 
 int main(int argc, char **argv)
 {
-ros::init(argc, argv, "pose_estimator");
-ros::NodeHandle n;
-sub = n.subscribe("/scan",1, positionCallback);
-ros::spin();
-return 0;
+    ros::init(argc, argv, "pose_estimator");
+    ros::NodeHandle n;
+    sub = n.subscribe("/scan",1, positionCallback);
+    ros::spin();
+    return 0;
 }
