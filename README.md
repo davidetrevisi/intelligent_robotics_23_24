3f2937e3f8f571870ca07be0a7275ccbf9060743
davidetrevisi <davide.trevisi19@gmail.com>
2023-12-04 14:34:27 +0100

    Comments and fixes

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
 # Intelligent Robotics 2023-2024
 
-Repository containing the solutions to the Intelligent Robotics exercises
+Repository containing solutions for Intelligent Robotics exercises
 
 ## Exercise 4
 ### Exercise 4.3
-To know in advance the size of the vector of data coming from the laser is sufficient to do vector_size = angle_max / angle_increment = 6.28 / 0.0087 = 720.
+To know in advance the size of the vector of data coming from the laser, it is sufficient to do: vector_size = angle_max / angle_increment = 6.28 / 0.0087 = 720.
 
-Conversion of the polar coordinates into cartesian (x,y) coordinates: x = r * cos(θ); y = r * sin(θ);
+Conversion of the polar coordinates to cartesian (x,y) coordinates: x = r * cos(θ); y = r * sin(θ);
 
-Each readout of the laser range finder converted into [x, y] coordinates is stored into leg_ranges vector;
+Each readout of the laser range finder converted to [x, y] coordinates is stored in the leg_ranges vector;
 
-To compute the position of each person we used a standard mathematical approach which consists in a for cycle iterating on each range found by the laser scan, if the range is not infinite (!std::isinf(ranges[i])) it is converted into (x,y) cartesian coordinates and then stored into a vector (leg_ranges), if the next range is infinite the program recognize that the points for this "leg" are finished and starts to store the point for the next leg in another vector until it reaches the end of the ranges (720). Then it computes the average of the points for each leg and the average for each person finding 3 positions:
+---
+
+NOTE: We made some assumptions for our solution: first, that each leg must be separated by at least one 'infinite' value of the laser scanner (e.g. no walls behind the people), then each leg is placed according to the given standard (e.g. each person stands in a circle around the laser and there are no overlapping legs or people turning their sides to the robot so that only one leg is actually read).
+
+It is possible to do this with the proposed "general solution" described below, but we decided to keep both methods instead.
+
+---
+
+To compute the position of each person we used a standard mathematical approach which consists in a "for" cycle iterating on each range found by the laser scan, if the range is not infinite (!std::isinf(ranges[i])) it is converted into (x,y) cartesian coordinates and then stored into a vector (leg_ranges), if the next range is infinite the program recognizes that the points for this "leg" are finished and starts to store the point for the next leg in another vector until it reaches the end of the ranges (720). Then it computes the average of the points for each leg and the average for each person, finding 3 positions:
 - Person 1 Position: (x, y) = (1.934557, -0.008540);
 - Person 2 Position: (x, y) = (1.071924, 2.020443);
 - Person 3 Position: (x, y) = (1.036030, -1.998724).
 
 
 ### Exercise 4.4
-To compute the position of any number of people, i.e. removing the assumption that there are three people, we used **kmeans** algorithm from the OpenCV library and selected *leg_count / 2* (computed using the previous algorithm) as **k**.
+To compute the position of any number of people, i.e. removing the assumption that there are three people, we used the kmeans algorithm from the OpenCV library and chose leg_count / 2 (computed with the previous algorithm) as k.
 Position of each person: 
 - (x, y) = (1.9355006, -0.0082151974);
 - (x, y) = (1.0901387, 2.0032334);
@@ -43,4 +51,4 @@ cd catkin_ws
 rosrun exercise_4 exercise_4
 ```
 
-OpenCV 4 is required, in theory it should have been already installed along ROS.
+OpenCV 4 is required, theoretically it should already be installed along with ROS.
--- src/exercise_4.cpp
+++ src/exercise_4.cpp
@@ -1,9 +1,12 @@
 #include <ros/ros.h>
+
 #include <sensor_msgs/LaserScan.h>
+
 #include <opencv2/core.hpp>
+
 #include <cmath>
 
-ros::Subscriber sub;
+// Global variables
 int leg_count = 0;
 int person_count = 0;
 int range_count[6] = {0};
@@ -11,31 +14,44 @@ float mid_x[6] = {0.0};
 float mid_y[6] = {0.0};
 float person_x[3] = {0.0};
 float person_y[3] = {0.0};
+
 std::vector<std::pair<float, float>> leg_ranges[6];
 std::vector<cv::Point2f> leg_ranges_cv;
 
+// Utility functions
 int pose_estimating();
 void array_shifting(float array[], int size);
+
+// Message callback function
 void positionCallback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
 {
+    // Save the message values
     std::vector<float> ranges = scan_msg->ranges;
     float angle_min = scan_msg->angle_min;
     float angle_increment = scan_msg->angle_increment;
+
+    // Cycle through all the values
     for (int i = 0; i < ranges.size(); i++)
     {
+        // If there is a leg (something is detected, so the value is not infinity)
         if (!std::isinf(ranges[i]))
         {
+            // Convert the value to [x, y] coordinates
             float angle = angle_min + (i * angle_increment);
             float x = ranges[i] * std::cos(angle);
             float y = ranges[i] * std::sin(angle);
 
+            // Add the point to the corresponding leg
             leg_ranges[leg_count].push_back(std::make_pair(x, y));
 
+            // Add the point to the values for the kmeans algorithm
             cv::Point2f point(x, y);
             leg_ranges_cv.push_back(point);
 
+            // Increment the counter
             range_count[leg_count]++;
 
+            // If we are at the last point of the leg, increase the leg counter
             if ((std::isinf(ranges[i + 1])) || i == ranges.size() - 1)
             {
                 leg_count++;
@@ -43,11 +59,13 @@ void positionCallback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
         }
     }
 
+    // Run kmeans clustering on the points
     cv::Mat centers;
     std::vector<int> labels;
 
     cv::kmeans(leg_ranges_cv, leg_count / 2, labels, cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 100, 0.001), 5, cv::KMEANS_PP_CENTERS, centers);
 
+    // Show the output
     ROS_INFO("Exercise 4.3 - Create a ROS node to compute the position of the people:");
     pose_estimating();
     ROS_INFO("Exercise 4.4 - Generalize the ROS node to compute the position of any number of people:");
@@ -58,33 +76,48 @@ void positionCallback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "pose_estimator");
+    // Create and initialize the node
+    ros::init(argc, argv, "exercise_4");
     ros::NodeHandle n;
-    sub = n.subscribe("/scan", 1, positionCallback);
+
+    // Subscribe to the topic
+    ros::Subscriber sub = n.subscribe("/scan", 1, positionCallback);
+
+    // Spin forever without rate limit
     ros::spin();
+
     return 0;
 }
 
 int pose_estimating()
 {
+    // Cycle through all the legs
     for (int l = 0; l < leg_count; l++)
     {
+        // Cycle through all the points
         for (int j = 0; j < range_count[l]; j++)
         {
             mid_x[l] += leg_ranges[l][j].first;
             mid_y[l] += leg_ranges[l][j].second;
         }
+        
+        // Compute the average [x, y] for the leg
         mid_x[l] = mid_x[l] / range_count[l];
         mid_y[l] = mid_y[l] / range_count[l];
+
+        // Every two legs, we have a person
         if ((l % 2 == 0))
         {
             person_count++;
         }
     }
 
+    // Shift the array to account for the person over the 'zero' marker:
+    // two legs at both ends of the array but in reality they are close
     array_shifting(mid_x, (sizeof(mid_x) / sizeof(mid_x[0])));
     array_shifting(mid_y, (sizeof(mid_x) / sizeof(mid_y[0])));
 
+    // Print the average for each person
     for (int i = 0; i < person_count; ++i)
     {
         person_x[i] = (mid_x[i * 2] + mid_x[i * 2 + 1]) / 2.0;
